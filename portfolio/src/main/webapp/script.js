b54b61ad24b00a52474c61f119222df4835a73fe
tlim045 <tlim045@e.ntu.edu.sg>
2020-06-08 22:29:17 +0800

    add fetch function.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
+
 /**
- * Adds a random greeting to the page.
+ * Another way to use fetch is by using the async and await keywords. This
+ * allows you to use the return values directly instead of going through
+ * Promises.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+async function getData() {
+  const response = await fetch('/data');
+  const data = await response.text();
+  document.getElementById('myData').innerText = data;
 }
+
